ca30542161292182f453a2980a63419429dba886
peridotadvisory <peridotadvisory@gmail.com>
2026-03-21 12:36:41 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Wealth-is-Not-a-Portfolio/",
+  base: '/Wealth-is-Not-a-Portfolio/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+    // Tailwind is not being actively used — do not remove them
     react(),
     tailwindcss(),
   ],
@@ -17,7 +17,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
